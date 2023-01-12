e0b1698f4ec7ba87a52445ab76f46fae0555803d
Suryanshu07 <45977317+Suryanshusingh123@users.noreply.github.com>
2023-01-12 15:31:46 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+int fact(int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    int smalloutput=fact(n-1);
+    return n*smalloutput;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int output =fact(n);
+    cout<<output;
+
+}
